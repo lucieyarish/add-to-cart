e06f9695441d7185a09112960de26b64a9c183c2
Lucie Yarish <lucieyarish@hotmail.com>
2023-10-29 07:09:13 +0100

    Feature: fetch and display added items from database in real time

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import {
   getDatabase,
   ref,
   push,
+  onValue,
 } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
 const appSettings = {
@@ -16,11 +17,37 @@ const shoppingListInDB = ref(database, 'shoppingList');
 
 const inputFieldEl = document.getElementById('input-field');
 const addBtnEl = document.getElementById('add-button');
+const shoppingListEl = document.getElementById('shopping-list');
 
 addBtnEl.addEventListener('click', function () {
   let inputValue = inputFieldEl.value;
 
   push(shoppingListInDB, inputValue);
-
-  console.log(`${inputValue} was added to database`);
+  clearInput();
 });
+
+onValue(shoppingListInDB, function (snapshot) {
+  let itemsInDb = Object.values(snapshot.val());
+
+  clearBooksListEl();
+
+  for (let i = 0; i < itemsInDb.length; i++) {
+    appendItemToShoppingList(itemsInDb[i]);
+  }
+});
+
+function clearInput() {
+  inputFieldEl.value = '';
+}
+
+function clearBooksListEl() {
+  shoppingListEl.innerHTML = '';
+}
+
+function appendItemToShoppingList(item) {
+  shoppingListEl.innerHTML += `
+    <li>
+        ${item}
+    </li>
+  `;
+}
